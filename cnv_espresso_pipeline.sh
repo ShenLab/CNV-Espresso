629c86aa9decaf519728655c884044bb08ac6614
microtan0902 <microtan0902@gmail.com>
2021-01-13 17:35:33 -0800

    update

--- cnv_espresso_pipeline.sh
+++ cnv_espresso_pipeline.sh
@@ -7,18 +7,26 @@ GATK_SW_DIR
 TARGET_PROBES
 REF_GENOME
 
-PROJECT_DIR='/home/rt2776/CNV_Espresso/'
-SCRIPTS_DIR='/home/rt2776/CNV_Espresso/src/'
+PROJECT_DIR='/home/rt2776/cnv_espresso/'
+SCRIPTS_DIR='/home/rt2776/cnv_espresso/src/'
 CNV_TOOLKIT_DIR='/home/rt2776/cnv_toolkit/scripts/'
 DATA_DIR='/home/rt2776/SPARK/CANOES/0-RC/spark_merged_RC/'
-OUTPUT_DIR='/home/rt2776/CNV_Espresso/result/NormReadCountRatio/'
+OUTPUT_DIR='/home/rt2776/cnv_espresso/result/NormReadCountRatio/'
 
 ```
 
-#### Step 1. Calculate RD/RC for each sample in the given cohort
+## Step 1. Calculate RC for each sample in the given cohort
+# Approach 1: using Mosdepth
+# Approach 2: by my script
+#TODO
 
-#### Step 2. Extract GC content for each interval (Not essential)
+## Step 2. Calculate correlation matrix
+#TODO
+#- note: we need to test/determing the correlation matrix calculation should be set  before or after GC and z-score normalization.
+
+## Step 3. Extract GC content for each interval (Not essential)
 ```
+#TODO
 java -Xmx2000m -Djava.io.tmpdir=${DATA_LOGS_DIR} \
                       -jar ${GATK_SW_DIR}GenomeAnalysisTK.jar \
                       -T GCContentByInterval \
@@ -26,6 +34,53 @@ java -Xmx2000m -Djava.io.tmpdir=${DATA_LOGS_DIR} \
                       -R ${REF_GENOME} \
                       -o ${DATA_DIR}gc.txt
 ```
+## Step 4. GC normalization, fit the negative binomial model and estimate mu and sigma, zscore 
+# just start with CLAMMS' windows.bed and Mosdepth cov.bed.gz files. We need to do it by ourselves later.
+sort -k1,1V -k2,2n windows.bed >windows_sort.bed
+
+python /home/rt2776/cnv_espresso/src/cnv_espresso.py normalization \
+    --windows /home/rt2776/cnv_espresso/data/windows_sort.bed \
+    --input /home/rt2776/cnv_espresso/data/ForCLAMMs/SP0000027.cov.bed.gz \
+    --output /home/rt2776/cnv_espresso/data/norm
+
+## alternatively, input a list with multiple samples 
+python /home/rt2776/cnv_espresso/src/cnv_espresso.py normalization \
+    --windows /home/rt2776/cnv_espresso/data/windows_sort.bed \
+    --input_list /home/rt2776/cnv_espresso/data/ref/SP0000027.ref.list \
+    --output /home/rt2776/cnv_espresso/data/norm
+
+## Normalize all the samples of SPARK WES1
+python /home/rt2776/cnv_espresso/src/cnv_espresso.py normalization \
+    --windows /home/rt2776/cnv_espresso/data/windows_sort.bed \
+    --input_list /home/rt2776/cnv_espresso/data/sample_rd.list \
+    --output /home/rt2776/cnv_espresso/data/norm
+### by cluster
+windows_file='/home/rt2776/cnv_espresso/data/windows_sort.bed'
+input_RD_list='/home/rt2776/cnv_espresso/data/sample_rd.list'
+output_dir='/home/rt2776/cnv_espresso/data/norm'
+qsub -tc 30 -t 1-28389 /home/rt2776/cnv_espresso/src/cluster_gc_norm.sh ${windows_file} ${input_RD_list} ${output_dir} 
+
+## Step 5. Select reference samples
+sample_list='/home/rt2776/cnv_espresso/source/spark_sample_27270.txt'
+sample_rd_dir='/home/rt2776/cnv_espresso/data/norm'
+
+### Open with Rstudio
+/home/rt2776/cnv_espresso/src/select_reference.Rmd
+
+## Step 6. Generate fixed-size windows for the CNV predicted regions
+### debuging by jupyter-lab
+/home/rt2776/cnv_espresso/src/generate_images.ipynb
+
+### finally running by cluster
+qsub -t 12000-58856 /home/rt2776/cnv_espresso/src/cluster_generate_images.sh \
+    /home/rt2776/cnv_espresso/training_set/training_set_true.txt 
+
+## Step 7. Deep Learning model
+
+
+
+#-------------------------------------------------------------------------------------------
+# Previous scripts in below
 
 #### Step 3a. Extract read count ratio for each sample
 ```
@@ -58,9 +113,3 @@ python ${SCRIPTS_DIR}cnv_espresso.py split \
     --input ${PROJECT_DIR}result/training_set_w_rc_clean.cnv \
     --output ${PROJECT_DIR}result/training_set_w_rc_equal_win.cnv \
     --num_target 3
-
-
-#### Step . Extract Read Count info for testing set
-
-
-#### Step 6. Build the Deep Learning model 
